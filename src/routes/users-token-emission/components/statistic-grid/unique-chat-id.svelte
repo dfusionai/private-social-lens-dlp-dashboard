5be38b467c311af08fa440d3a90a594a15b8e81c
linhtcn <linhtcn@var-meta.com>
2025-07-09 14:08:55 +0700

    fix: resolving timeout issue

--- src/routes/users-token-emission/components/statistic-grid/unique-chat-id.svelte
+++ src/routes/users-token-emission/components/statistic-grid/unique-chat-id.svelte
@@ -3,6 +3,7 @@
     import * as Chart from "$lib/components/ui/chart/index.js";
     import { onMount } from "svelte";
     import DonutChart from "$lib/components/common/donut-chart/donut-chart.svelte";
+    import EmptyDonutChart from "$lib/components/common/empty-donut-chart/empty-donut-chart.svelte";
     import { Text } from "layerchart";
     import {
         formatDate,
@@ -14,9 +15,11 @@
         type INewChatsPerDayItem,
     } from "../../../../api/fetchNewChatsPerDay";
     import { fetchRefreshedChatsCount } from "../../../../api/fetchRefreshedChatsCount";
-    import EmptyDonutChart from "$lib/components/common/empty-donut-chart/empty-donut-chart.svelte";
+    import { Button } from "$lib/components/ui/button";
+    import { RefreshCwIcon } from "@lucide/svelte";
+
     const getSum = (data: INewChatsPerDayItem[]) => {
-        return data.reduce((acc, curr) => acc + curr.newChats, 0);
+        return data.reduce((acc, curr) => acc + (curr.newChats || 0), 0);
     };
 
     let monthData = generateDailyChartData(fromIndex, daysPerMonth);
@@ -32,13 +35,11 @@
         refreshChats: 0,
     });
 
-    const isEmpty = $derived(
-        chatData.newChats === 0 && chatData.refreshChats === 0
-    );
+    const isEmpty = $derived(chatData.totalUniqueChats === 0);
 
     let isLoading = $state(false);
 
-    onMount(async () => {
+    const queryHandler = async () => {
         try {
             isLoading = true;
             const refreshedChatCount = await fetchRefreshedChatsCount(params);
@@ -53,9 +54,13 @@
         } finally {
             isLoading = false;
         }
+    };
+
+    onMount(() => {
+        queryHandler();
     });
 
-    const chartData = [
+    const chartData = $derived([
         {
             key: "newChats",
             value: chatData.newChats,
@@ -66,39 +71,48 @@
             value: chatData.refreshChats,
             color: "var(--color-refreshChats)",
         },
-    ];
+    ]);
 
     const chartConfig = {
-        newChats: { label: "newChats", color: "var(--chart-1)" },
-        refreshChats: { label: "refreshChats", color: "var(--chart-2)" },
+        newChats: { label: "New Chats", color: "var(--chart-1)" },
+        refreshChats: { label: "Refresh Chats", color: "var(--chart-2)" },
     } satisfies Chart.ChartConfig;
 </script>
 
-{#if isEmpty}
-    <EmptyDonutChart title="Total Unique Chat IDs" />
-{:else}
-    <DonutChart
-        {isLoading}
-        {chartConfig}
-        data={chartData}
-        title="Total Unique Chat IDs"
-        description="Breakdown of new vs refresh chats in the network"
+<div class="relative">
+    {#if isEmpty && !isLoading}
+        <EmptyDonutChart title="Total Unique Chat IDs" />
+    {:else}
+        <DonutChart
+            {isLoading}
+            {chartConfig}
+            data={chartData}
+            title="Total Unique Chat IDs"
+            description="Breakdown of new vs refresh chats in the network"
+        >
+            <g slot="center" transform="translate(0, 0)">
+                <Text
+                    value={formatNumberIntoShort(chatData.totalUniqueChats)}
+                    textAnchor="middle"
+                    verticalAnchor="middle"
+                    class="fill-foreground text-3xl! font-bold"
+                    dy={3}
+                />
+                <Text
+                    value="Total chats"
+                    textAnchor="middle"
+                    verticalAnchor="middle"
+                    class="fill-muted-foreground! text-muted-foreground"
+                    dy={22}
+                />
+            </g>
+        </DonutChart>
+    {/if}
+    <Button
+        class="bg-transparent cursor-pointer hover:bg-background absolute top-6 right-6"
+        disabled={isLoading}
+        onclick={queryHandler}
     >
-        <g slot="center" transform="translate(0, 0)">
-            <Text
-                value={formatNumberIntoShort(chatData.totalUniqueChats)}
-                textAnchor="middle"
-                verticalAnchor="middle"
-                class="fill-foreground text-3xl! font-bold"
-                dy={3}
-            />
-            <Text
-                value="Total chats"
-                textAnchor="middle"
-                verticalAnchor="middle"
-                class="fill-muted-foreground! text-muted-foreground"
-                dy={22}
-            />
-        </g>
-    </DonutChart>
-{/if}
+        <RefreshCwIcon class="h-4 w-4 text-foreground" />
+    </Button>
+</div>
--- src/routes/users-token-emission/components/statistic-grid/user-distribution.svelte
+++ src/routes/users-token-emission/components/statistic-grid/user-distribution.svelte
@@ -5,20 +5,31 @@
     import DonutChart from "$lib/components/common/donut-chart/donut-chart.svelte";
     import { Text } from "layerchart";
     import EmptyDonutChart from "$lib/components/common/empty-donut-chart/empty-donut-chart.svelte";
+    import Button from "$lib/components/ui/button/button.svelte";
+    import { RefreshCwIcon } from "@lucide/svelte";
+
     let chatData = $state({
         totalUsers: 0,
         webAppUsers: 0,
         minerAppUsers: 0,
     });
 
-    onMount(async () => {
+    let isLoading = $state(false);
+
+    const queryHandler = async () => {
         try {
         } catch (error) {
             throw error;
+        } finally {
+            isLoading = false;
         }
+    };
+
+    onMount(() => {
+        queryHandler();
     });
 
-    const chartData = [
+    const chartData = $derived([
         {
             key: "webAppUsers",
             value: chatData.webAppUsers,
@@ -29,42 +40,49 @@
             value: chatData.minerAppUsers,
             color: "var(--color-minerAppUsers)",
         },
-    ];
+    ]);
 
-    const isEmpty = $derived(
-        chatData.webAppUsers === 0 && chatData.minerAppUsers === 0
-    );
+    const isEmpty = $derived(chatData.totalUsers === 0);
 
     const chartConfig = {
-        webAppUsers: { label: "webAppUsers", color: "var(--chart-1)" },
-        minerAppUsers: { label: "minerAppUsers", color: "var(--chart-2)" },
+        webAppUsers: { label: "Web App Users", color: "var(--chart-1)" },
+        minerAppUsers: { label: "Miner App Users", color: "var(--chart-2)" },
     } satisfies Chart.ChartConfig;
 </script>
 
-{#if isEmpty}
-    <EmptyDonutChart title="Total Users" />
-{:else}
-    <DonutChart
-        {chartConfig}
-        data={chartData}
-        title="Total Users"
-        description="Breakdown of webApp vs minerApp users in the network"
+<div class="relative">
+    {#if isEmpty && !isLoading}
+        <EmptyDonutChart title="Total Users" />
+    {:else}
+        <DonutChart
+            {chartConfig}
+            data={chartData}
+            title="Total Users"
+            description="Breakdown of webApp vs minerApp users in the network"
+        >
+            <g slot="center" transform="translate(0, 0)">
+                <Text
+                    value={formatNumberIntoShort(chatData.totalUsers)}
+                    textAnchor="middle"
+                    verticalAnchor="middle"
+                    class="fill-foreground text-3xl! font-bold"
+                    dy={3}
+                />
+                <Text
+                    value="Total users"
+                    textAnchor="middle"
+                    verticalAnchor="middle"
+                    class="fill-muted-foreground! text-muted-foreground"
+                    dy={22}
+                />
+            </g>
+        </DonutChart>
+    {/if}
+    <Button
+        class="bg-transparent cursor-pointer hover:bg-background absolute top-6 right-6"
+        disabled={isLoading}
+        onclick={queryHandler}
     >
-        <g slot="center" transform="translate(0, 0)">
-            <Text
-                value={formatNumberIntoShort(chatData.totalUsers)}
-                textAnchor="middle"
-                verticalAnchor="middle"
-                class="fill-foreground text-3xl! font-bold"
-                dy={3}
-            />
-            <Text
-                value="Total users"
-                textAnchor="middle"
-                verticalAnchor="middle"
-                class="fill-muted-foreground! text-muted-foreground"
-                dy={22}
-            />
-        </g>
-    </DonutChart>
-{/if}
+        <RefreshCwIcon class="h-4 w-4 text-foreground" />
+    </Button>
+</div>
